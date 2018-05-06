8d7cb44025384c0fa7f5856c0b7ab18819d08b77
aleksei-vasiluev <39021831+aleksei-vasiluev@users.noreply.github.com>
2018-05-06 13:23:20 +0300

    Update array_in_bordersql.php

--- array_in_bordersql.php
+++ array_in_bordersql.php
@@ -1,24 +1,24 @@
 <?php
 class array_in_bordersql
 {
-    public $mass_array = array('pavel', 'aleksei', 'petya');
+    public $mass_array = array('key_1'=>'pavel', 'key_2'=>'aleksei','key_3'=>'petya');
     public $array;
     public $border='';
-    public $str= strlen($array);
+    //public $str= strlen($value);
 
     public function tabledrop()
     {
-        for(;*str;++str){
+        for($i=0; $i<$str; $border++){
         echo "+".$border."+";
         }
     }
 
     public function array_elem()
     {
-        for(enumeration of array elements){
+        for($i=0;$i<mass_array; $mass_array++){
             echo "| ".$array." |";
         }
     }
 }
 
-$mass = new (array_in_bordersql);
+$mass = new array_in_bordersql();
